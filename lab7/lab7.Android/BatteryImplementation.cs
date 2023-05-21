97921e622ea2e38463a216ab465baf7ada1b345b
aracely-huamani <102335298+aracely-huamani@users.noreply.github.com>
2023-05-21 18:20:02 -0500

    Agregar archivos de proyecto.

---
+++ lab7/lab7.Android/BatteryImplementation.cs
@@ -0,0 +1,138 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using lab7.Droid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+[assembly: Xamarin.Forms.Dependency(typeof(BatteryImplementation))]
+
+namespace lab7.Droid
+{
+    public class BatteryImplementation : IBattery
+    {
+        public BatteryImplementation()
+        {
+        }
+
+        public int RemainingChargePercent
+        {
+            get
+            {
+                try
+                {
+                    using (var filter = new IntentFilter(Intent.ActionBatteryChanged))
+                    {
+                        using (var battery = Application.Context.RegisterReceiver(null, filter))
+                        {
+                            var level = battery.GetIntExtra(BatteryManager.ExtraLevel, -1);
+                            var scale = battery.GetIntExtra(BatteryManager.ExtraScale, -1);
+
+                            return (int)Math.Floor(level * 100D / scale);
+                        }
+                    }
+                }
+                catch
+                {
+                    System.Diagnostics.Debug.WriteLine("Ensure you have android.permission.BATTERY_STATS");
+                    throw;
+                }
+            }
+        }
+
+        public lab7.BatteryStatus Status
+        {
+            get
+            {
+                try
+                {
+                    using (var filter = new IntentFilter(Intent.ActionBatteryChanged))
+                    {
+                        using (var battery = Application.Context.RegisterReceiver(null, filter))
+                        {
+                            int status = battery.GetIntExtra(BatteryManager.ExtraStatus, -1);
+                            var isCharging = status == (int)BatteryStatus.Charging || status == (int)BatteryStatus.Full;
+
+                            var chargePlug = battery.GetIntExtra(BatteryManager.ExtraPlugged, -1);
+                            var usbCharge = chargePlug == (int)BatteryPlugged.Usb;
+                            var acCharge = chargePlug == (int)BatteryPlugged.Ac;
+                            bool wirelessCharge = false;
+                            wirelessCharge = chargePlug == (int)BatteryPlugged.Wireless;
+
+                            isCharging = (usbCharge || acCharge || wirelessCharge);
+                            if (isCharging)
+                                return lab7.BatteryStatus.Charging;
+
+                            switch (status)
+                            {
+                                case (int)BatteryStatus.Charging:
+                                    return lab7.BatteryStatus.Charging;
+                                case (int)BatteryStatus.Discharging:
+                                    return lab7.BatteryStatus.Discharging;
+                                case (int)BatteryStatus.Full:
+                                    return lab7.BatteryStatus.Full;
+                                case (int)BatteryStatus.NotCharging:
+                                    return lab7.BatteryStatus.NotCharging;
+                                default:
+                                    return lab7.BatteryStatus.Unknown;
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+                    System.Diagnostics.Debug.WriteLine("Ensure you have android.permission.BATTERY_STATS");
+                    throw;
+                }
+            }
+        }
+
+        public PowerSource PowerSource
+        {
+            get
+            {
+                try
+                {
+                    using (var filter = new IntentFilter(Intent.ActionBatteryChanged))
+                    {
+                        using (var battery = Application.Context.RegisterReceiver(null, filter))
+                        {
+                            int status = battery.GetIntExtra(BatteryManager.ExtraStatus, -1);
+                            var isCharging = status == (int)BatteryStatus.Charging || status == (int)BatteryStatus.Full;
+
+                            var chargePlug = battery.GetIntExtra(BatteryManager.ExtraPlugged, -1);
+                            var usbCharge = chargePlug == (int)BatteryPlugged.Usb;
+                            var acCharge = chargePlug == (int)BatteryPlugged.Ac;
+
+                            bool wirelessCharge = false;
+                            wirelessCharge = chargePlug == (int)BatteryPlugged.Wireless;
+
+                            isCharging = (usbCharge || acCharge || wirelessCharge);
+
+                            if (!isCharging)
+                                return lab7.PowerSource.Battery;
+                            else if (usbCharge)
+                                return lab7.PowerSource.Usb;
+                            else if (acCharge)
+                                return lab7.PowerSource.Ac;
+                            else if (wirelessCharge)
+                                return lab7.PowerSource.Wireless;
+                            else
+                                return lab7.PowerSource.Other;
+                        }
+                    }
+                }
+                catch
+                {
+                    System.Diagnostics.Debug.WriteLine("Ensure you have android.permission.BATTERY_STATS");
+                    throw;
+                }
+            }
+        }
+    }
+}
---
+++ lab7/lab7/ITextToSpeech.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab7
+{
+    public interface ITextToSpeech
+    {
+        void Speak(string text);
+    }
+}
